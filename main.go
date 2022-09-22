6f8333f395da0e2c34f3d9b16e7443a38bb4c575
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-22 23:59:33 +0700

    Cách sử dụng switch-case trong Go.

--- main.go
+++ main.go
@@ -21,4 +21,22 @@ func main() {
 	} else {
 		fmt.Println("a = 100")
 	}
+	//--switch - case: Không cần sử dụng break
+	number1 := 5
+	switch number1 {
+	case 1:
+		fmt.Println("number = 1")
+	case 2:
+		fmt.Println("number = 2")
+	case 3:
+		fmt.Println("number = 3")
+	case 4:
+		fmt.Println("number = 4")
+	case 5, 6, 7, 8, 9:
+		fmt.Println("number = 5,6,7,8,9")
+	case 10:
+		fmt.Println("number = 10")
+	default:
+		fmt.Println("Undefined")
+	}
 }
